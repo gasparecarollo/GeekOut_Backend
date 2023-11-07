6fc61e87fe5507be3cbdaad6d7396a1cded87362
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-11-07 16:40:30 -0500

    Modified validations

--- validations/checkGames.js
+++ validations/checkGames.js
@@ -4,8 +4,8 @@ const checkName = (req, res, next) => {
     const name = req.body.name;
 
     if (name) {
-
         next()
+
     } else {
         res.status(400).json({ error: "Name is required" })
     }
