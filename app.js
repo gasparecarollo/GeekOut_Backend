53e5c78361d15a770f3ef81dc50f33bbc01e2f22
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-11-06 19:09:36 -0500

    Uncommented App.js

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-// //DEPENDENCIES
-// const express = require('express');
-// const app = express();
-// const cors = require('cors');
+//DEPENDENCIES
+const express = require('express');
+const app = express();
+const cors = require('cors');
 
-// //MIDDLEWARE
-// app.use(cors());
-// app.use(express.json());
-// app.use('/games')
+//MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+app.use('/games')
 
 
-// //ROUTES
-// app.get('/', (req, res) => {
-//     res.send('Welcome to Geek Out')
-// });
+//ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to Geek Out')
+});
 
-// app.get('*', (req, res) => {
-//     res.status(404).send("Nadda")
-// })
+app.get('*', (req, res) => {
+    res.status(404).send("Nadda")
+})
