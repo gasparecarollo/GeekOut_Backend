8e8003ecd63063d570eba11b01e05a281b0db247
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-11-08 11:51:49 -0500

    Added youtube video ids

--- db/seed.sql
+++ db/seed.sql
@@ -3,23 +3,23 @@
 INSERT INTO games (name,is_favorite,genre,storyline,video_id,image_id,cost) VALUES 
 ('Home Alone', TRUE, 'Action', 'Kevin McCallister is left home alone when his family goes on Christmas vacation to Paris. He must prevent Harry and Marv, the "Wet Bandits", from burgling his home, using various household objects as traps and/or weapons.', 'kj9et5aSVN8', 'image_id_11', 22),
 
-('Road Rash', TRUE, 'Racing, Vehicular Combat', 'Road Rash puts the player in control of a motorcycle racer who must finish in fourth place or higher among fourteen other racers; the player advances by winning five races on each level. The game is primarily single-player, but allows for two players to play intermittently against each other.', 'video_id_11', 'image_id_11', 39),
+('Road Rash', TRUE, 'Racing, Vehicular Combat', 'Road Rash puts the player in control of a motorcycle racer who must finish in fourth place or higher among fourteen other racers; the player advances by winning five races on each level. The game is primarily single-player, but allows for two players to play intermittently against each other.', 'kItq4rXd0fI', 'image_id_11', 39),
 
-('Sonic The Hedgehog 1', TRUE, 'Platform', 'Sonic the Hedgehog is a 2D side-scrolling platform game. The gameplay centers on Sonic the Hedgehogs ability to run at high speed through levels that include springs, slopes, bottomless pits, and vertical loops and the levels are populated with robot enemies.', 'video_id_11', 'image_id_11', 59),
+('Sonic The Hedgehog 1', TRUE, 'Platform', 'Sonic the Hedgehog is a 2D side-scrolling platform game. The gameplay centers on Sonic the Hedgehogs ability to run at high speed through levels that include springs, slopes, bottomless pits, and vertical loops and the levels are populated with robot enemies.', 'Gsa_4s3CjmI', 'image_id_11', 59),
 
-('Croc', TRUE, 'Platform',  'Croc: Legend of the Gobbos is a third-person 3D platformer in which the player controls the main character, a green crocodile named Croc, through several courses taking place on various islands throughout Gobbo Valley.', 'video_id_11', 'image_id_11', 19),
+('Croc', TRUE, 'Platform',  'Croc: Legend of the Gobbos is a third-person 3D platformer in which the player controls the main character, a green crocodile named Croc, through several courses taking place on various islands throughout Gobbo Valley.', 'ByxGZp2di_E', 'image_id_11', 19),
 
-('Spyro', TRUE, 'Platform', 'A young cute purple dragon named Spyro sets out to free his dragon buddies, after they had fallen victim to the villainous Gnasty Gnorc, who has turned them into crystal statues.', 'video_id_11', 'image_id_11', 12),
+('Spyro', TRUE, 'Platform', 'A young cute purple dragon named Spyro sets out to free his dragon buddies, after they had fallen victim to the villainous Gnasty Gnorc, who has turned them into crystal statues.', 'HmatxYZtTD0', 'image_id_11', 12),
 
 ('Prince of Persia', TRUE, 'Cinematic platformer/
-Action-adventure', 'Together with Farah, a young Princess who knows the power of the Sands, the Prince works to correct his mistake and foil the Viziers evil plans.', 'video_id_11', 'image_id_11', 11),
+Action-adventure', 'Together with Farah, a young Princess who knows the power of the Sands, the Prince works to correct his mistake and foil the Viziers evil plans.', 'd5as9axJD1U', 'image_id_11', 11),
 
-('Divinity: Original Sin 2', TRUE, 'Role-Playing', 'The player character, a Sourcerer, is captured by the Divine Order and transported to the island prison Fort Joy on the ship Merryweather.', 'video_id_11', 'image_id_11', 12),
+('Divinity: Original Sin 2', TRUE, 'Role-Playing', 'The player character, a Sourcerer, is captured by the Divine Order and transported to the island prison Fort Joy on the ship Merryweather.', 'unWFWGrMbpc', 'image_id_11', 12),
 
 ('Maniac Mansion', TRUE, 'Graphic Adventure', 'Maniac Mansion is a graphic adventure game in which the player uses a point-and-click interface to guide characters through a two-dimensional game world and to solve puzzles.', 'wH_GX_w4rK4', 'image_id_11', 24),
 
-('ICO', TRUE, 'Action-Adventure', 'Ico is primarily a three-dimensional platform game. The player controls Ico from a third-person perspective as he explores the castle and attempts to escape it with Yorda.', 'video_id_11', 'image_id_11', 23),
+('ICO', TRUE, 'Action-Adventure', 'Ico is primarily a three-dimensional platform game. The player controls Ico from a third-person perspective as he explores the castle and attempts to escape it with Yorda.', 'JBm-RRm6-TQ_id_11', 'image_id_11', 23),
 
 ('Zombies Ate My Neighbors', TRUE, 'Run and Gun',  'Zeke and Julie, equipped with a variety of weapons and power-ups, go on a quest to rescue their neighbors from monsters often seen in horror movies.', 'jbsreeNpBHo', 'image_id_11', 16),
 
-('God of War', TRUE, 'Hack and Slash', 'In God of War, players control Kratos, a Spartan warrior who is sent by the Greek gods to kill Ares, the god of war.', 'video_id_11', 'image_id_11', 20)
+('God of War', TRUE, 'Hack and Slash', 'In God of War, players control Kratos, a Spartan warrior who is sent by the Greek gods to kill Ares, the god of war.', 'CJ_GCPaKywg', 'image_id_11', 20)
